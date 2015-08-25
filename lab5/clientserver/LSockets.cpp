7f055293f009f77256c8bd9b7053ca972704dff7
luke lincoln <lukes611@gmail.com>
2015-08-25 10:03:29 +1000

    adding more

--- lab5/clientserver/LSockets.cpp
+++ lab5/clientserver/LSockets.cpp
@@ -11,22 +11,75 @@
 using namespace std;
 
 
-LServer::LServer(){}
-LServer::LServer(int port){}
-void LServer::error(string er){}
-void LServer::open(int port){}
-void LServer::close(){}
-string LServer::read(int max = 2048)
-{
-	
-} 
-void LServer::write(std::string data){}
-void LServer::listen()
+LServer::LServer()
 {
 	
 }
+LServer::LServer(const LServer & in)
+{
+	port = in.port;
+	primary = in.primary;
+	client = in.client;
+}
+LServer & LServer::operator = (const LServer & in)
+{
+	if(this == &in) return *this;
+	port = in.port;
+	primary = in.primary;
+	client = in.client;
+	return *this;
+}
+LServer::LServer(int port)
+{
+	open(port);
+}
+void LServer::error(string er)
+{
+	cout << er << endl;
+	exit(5);
+}
+void LServer::open(int port)
+{
+	this->port = port;
+	setup();
+}
+void LServer::shutdown()
+{
+	end();
+	close(primary);
+}
+void LServer::end()
+{
+	close(client);
+}
+string LServer::read(int max)
+{
+	char * tmp = new char[max];
+	int N = recv(client, tmp, max, 0);
+	tmp[N] = 0x00;
+	string rv = tmp;
+	delete [] tmp;
+	return rv;
+} 
+void LServer::write(std::string data)
+{
+	send(client, data.c_str(), data.size(), 0);
+}
+void LServer::wait_for_client()
+{
+	struct sockaddr_in cli_name;
+	socklen_t clilen;
+	client = accept(primary, (sockaddr*) &cli_name, &clilen);
+}
 void LServer::setup()
 {
+	struct sockaddr_in name;
+	primary = socket(AF_INET, SOCK_STREAM, 0);
+	if(primary < 0) error("error creating socket in LSocket::setup");
 	
-	
+	name.sin_family = AF_INET;
+	name.sin_addr.s_addr = (INADDR_ANY);
+	name.sin_port = htons((short)port);
+	if(bind(primary, (sockaddr*) &name, sizeof(name)) < 0) error ( "LSocket::setup() -> error binding" );
+	listen(primary, 5);
 }
--- lab5/clientserver/LSockets.h
+++ lab5/clientserver/LSockets.h
@@ -6,12 +6,15 @@ public:
 	int port, primary, client;
 	LServer();
 	LServer(int port);
+	LServer(const LServer & in);
+	LServer & operator = (const LServer & in);
 	void error(std::string er); //closes program
 	void open(int port); //opens the connection
-	void close(); //closes the connection
+	void shutdown(); //closes all communication
+	void end();  //ends communication with client
 	std::string read(int max = 2048); //reads in a string with maximum chars returned
 	void write(std::string data);
-	void listen();
+	void wait_for_client();
 private:
 	void setup();
 };
